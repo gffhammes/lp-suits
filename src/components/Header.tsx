78795ac2454867b30cd0ec711ff4d07498482526
Guilherme Hammes <gffhammes@gmail.com>
2024-09-19 20:57:25 -0300

    feat: adjust header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,60 @@
-import { Box, Typography } from "@mui/material";
+"use client";
+
+import {
+  Box,
+  Container,
+  Slide,
+  Typography,
+  useScrollTrigger,
+} from "@mui/material";
+import logo from "../../public/images/logo suits branca 01 3.png";
+import Image from "next/image";
 
 export interface IHeaderProps {}
 
 export const Header = (props: IHeaderProps) => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 200,
+  });
+
   return (
-    <Box sx={{ position: "fixed", top: 0, zIndex: 999 }}>
-      <Typography>header</Typography>
-    </Box>
+    <>
+      <Container sx={{ position: "absolute", zIndex: 999, pt: 5 }}>
+        <Image
+          src={logo.src}
+          height={100}
+          width={100}
+          alt="Logo Suits"
+          style={{ objectFit: "contain" }}
+        />
+      </Container>
+
+      <Slide in={trigger}>
+        <Box
+          sx={{
+            width: "100%",
+            position: "fixed",
+            top: 0,
+            zIndex: 999,
+            backgroundColor: "rgba(30, 30, 30,.9)",
+            backdropFilter: "blur(20px)",
+            borderBottom: "1px solid #3c3c3c",
+            py: 1,
+            transition: ".3s ease all",
+          }}
+        >
+          <Container>
+            <Image
+              src={logo.src}
+              height={40}
+              width={40}
+              alt="Logo Suits"
+              style={{ objectFit: "contain", transition: ".3s ease all" }}
+            />
+          </Container>
+        </Box>
+      </Slide>
+    </>
   );
 };
