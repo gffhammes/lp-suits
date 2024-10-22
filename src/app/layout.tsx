2e2e2f909146a39efaf638d6704833d3ddb005a0
Guilherme Hammes <gffhammes@gmail.com>
2024-10-22 11:04:06 -0300

    feat: try to add two gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,14 +22,46 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <Head>
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TRBSTKML');`}
+        </script>
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MPDH9DPN');`}
+        </script>
+      </Head>
+
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TRBSTKML"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MPDH9DPN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </AppRouterCacheProvider>
       </body>
 
-      <GoogleTagManager gtmId="GTM-MPDH9DPN" />
-      <GoogleAnalytics gaId="G-D8EH0M00KS" />
+      {/* <GoogleTagManager gtmId="GTM-MPDH9DPN" /> */}
+      {/* <GoogleAnalytics gaId="G-D8EH0M00KS" /> */}
 
       <Script
         id="microsoft-clarity-init"
